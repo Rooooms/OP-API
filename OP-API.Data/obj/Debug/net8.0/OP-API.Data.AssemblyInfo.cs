ebfc7a9502efd1ed4d00d5a1a597149acbbc4067
Romeo Rojo <romeorojo45@gmail.com>
2024-06-27 14:35:44 +0800

    update

--- OP-API.Data/obj/Debug/net8.0/OP-API.Data.AssemblyInfo.cs
+++ OP-API.Data/obj/Debug/net8.0/OP-API.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OP-API.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eaa8ceb8885bc12d9228453e1ad3d53f16932cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c63f6f63009097002f85a8d64ab2ad3755d3177")]
 [assembly: System.Reflection.AssemblyProductAttribute("OP-API.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OP-API.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
